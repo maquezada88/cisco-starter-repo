8a5c3f221662b4181d408cc74933b30133bc450e
maquezada88 <107223686+maquezada88@users.noreply.github.com>
2023-01-30 23:39:34 +0000

    2nd task completed

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <FancyBorder color="blue">
+    <h1 className="Dialog-title">
+      Welcome
+    </h1>
+    <p className="Dialog-message">
+      Thank you for visiting our spacecraft!
+    </p>
+  </FancyBorder>
+  );
+}
+
+function FancyBorder(props) {
+  return (
+    <div className={'FancyBorder FancyBorder-' + props.color}>
+      {props.children}
     </div>
   );
 }
+
 
 export default App;
