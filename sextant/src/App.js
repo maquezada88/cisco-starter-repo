10e78d0ad5bd6d46e76aa1355f1e4191a3a95653
maquezada88 <107223686+maquezada88@users.noreply.github.com>
2023-01-31 18:44:35 +0000

    ip task

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+import {useState,useEffect} from 'react'
 import Banner from './Banner';
 import Exhibit from './Exhibit';
 
 class App extends Component {
+
     render() {
+        //creating IP state
+        const [ip, setIP] = useState('');
+      
+        //creating function to load ip address from the API
+        const getData = async () => {
+          const res = await axios.get('https://geolocation-db.com/json/')
+          console.log(res.data);
+          setIP(res.data.IPv4)
+        }
+        
+        useEffect( () => {
+          //passing getData method to the lifecycle method
+          getData()
+      
+        }, [])
         return (
             <div className="App">
-                <Banner bannerText="Sextant" />
-                <Exhibit name="I'm an exhibit!"></Exhibit>
-                <Exhibit name="I'm also an exhibit!"></Exhibit>
-                <Exhibit name="Don't forget about me, I'm an exhibit too!"></Exhibit>
+                <Banner bannerText="CISCO PROJECT" />
+                <Exhibit name="I'm an exhibit!">{ip}</Exhibit>
             </div>
         );
     }
