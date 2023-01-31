e7ac64e7d68ad555440cbd008a4fc124cd621dd1
maquezada88 <107223686+maquezada88@users.noreply.github.com>
2023-01-31 18:50:35 +0000

    hi

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,31 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import {useState,useEffect} from 'react'
 import Banner from './Banner';
 import Exhibit from './Exhibit';
+import AddressDisplay from './AddressDisplay';
 
 class App extends Component {
-
     render() {
-        //creating IP state
-        const [ip, setIP] = useState('');
-      
-        //creating function to load ip address from the API
-        const getData = async () => {
-          const res = await axios.get('https://geolocation-db.com/json/')
-          console.log(res.data);
-          setIP(res.data.IPv4)
-        }
-        
-        useEffect( () => {
-          //passing getData method to the lifecycle method
-          getData()
-      
-        }, [])
         return (
             <div className="App">
-                <Banner bannerText="CISCO PROJECT" />
-                <Exhibit name="I'm an exhibit!">{ip}</Exhibit>
+                <Banner bannerText="Sextant" />
+                <Exhibit name="Public IPv4 Address">
+                    <AddressDisplay url='https://api.ipify.org?format=json' />
+                </Exhibit>
+                <Exhibit name="Public IPv6 Address">
+                    <AddressDisplay url='https://api64.ipify.org?format=json' />
+                </Exhibit>
             </div>
         );
     }
